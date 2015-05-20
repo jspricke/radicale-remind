8585e897c1a8fed7bf0877281f7f4acebdaaafe8
Jochen Sprickerhof <git@jochen.sprickerhof.de>
2015-05-20 11:52:49 +0200

    Version 0.1.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='radicale-remind',
-      version='0.1.0',
+      version='0.1.1',
       description='''
        Radicale Remind and Abook storage backend
        ''',
