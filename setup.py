964101c7f996a16e0e326b25306d877ded148ed7
Jochen Sprickerhof <git@jochen.sprickerhof.de>
2025-04-25 08:57:12 +0200

    Version 0.7.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="radicale-remind",
-    version="0.7.0",
+    version="0.7.1",
     description="Radicale Remind, Abook and Taskwarrior storage backend",
     long_description=open("README.rst").read(),
     author="Jochen Sprickerhof",
